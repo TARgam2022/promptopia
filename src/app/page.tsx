4ef6187c040f9da5ed0409e0acfb1286558f57a3
Thomas Alzate <targam2021@gmail.com>
2024-05-07 20:56:44 -0500

    Starter Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import "../styles/page.scss";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="header">hola</h1>
+      <h1>hello World</h1>
+    </main>
+  );
+}
