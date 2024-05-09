c6187f16ae7f483dc5de5ab7caa7c0f852a6634f
Thomas Alzate <targam2021@gmail.com>
2024-05-08 20:45:24 -0500

    Project setuped and starting to work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import "../styles/page.scss";
+import "../styles/pages/hero.scss";
+import "@/styles/utilities/miscs.scss";
+import { Navbar } from "@/components/navbar";
 
 export default function Home() {
   return (
     <main>
-      <h1 className="header">hola</h1>
-      <h1>hello World</h1>
+      <Navbar />
     </main>
   );
 }
